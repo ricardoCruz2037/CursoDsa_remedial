77e07768127288d877425cb800de048498b2230a
Ricardo López Cruz <230110088@itsoeh.edu.mx>
2024-12-10 21:09:35 -0600

    test

--- learning/Stacks/stack_sandbox.py
+++ learning/Stacks/stack_sandbox.py
@@ -2,7 +2,19 @@ from Node import Node
 import logging
 class Stack:
     def __init__(self, top_item = None):
+        
         self.top_item = top_item
+ 
+    def push(self, value):
+       #2.1
+        item = Node(value)
+        item.set_next_node(self.top_item)
+        self.top_item = item
     
+    def pop(self):
+        item_to_remove = self.top_item
+        self.top_item = self.top_item.get_next_node()
+        return item_to_remove.get_value()
+           
     def peek(self):
         return self.top_item.get_value()
